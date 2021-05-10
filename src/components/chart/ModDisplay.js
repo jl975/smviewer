223e4b1aa59448697c8af0837425367dbcbf7f08
Jay Lee <j.lee2@adheretech.com>
2021-05-10 00:35:10 -0700

    blank mod icon with cmod/mmod support

--- src/components/chart/ModDisplay.js
+++ src/components/chart/ModDisplay.js
@@ -5,6 +5,8 @@ import { getAssetPath } from '../../utils'
 
 const icons = {}
 
+icons['blank'] = new Image()
+icons['blank'].src = getAssetPath(`mods/blank.png`)
 options.mods.speed.forEach((speedMod) => {
   const imageName = `speed_x${speedMod.toString().replace('.', '_')}`
   icons[speedMod] = new Image()
@@ -22,6 +24,17 @@ const ModDisplay = (props) => {
   console.log(mods)
 
   const renderSpeedMod = () => {
+    if (mods.speed === 'cmod' || mods.speed === 'mmod') {
+      return (
+        <>
+          <img src={icons['blank'].src} />
+          <div className="custom-mod">
+            {mods.speed === 'mmod' ? 'M' : 'C'}
+            {mods.cmod}
+          </div>
+        </>
+      )
+    }
     if (!mods.speed || isNaN(mods.speed)) {
       return null
     }
