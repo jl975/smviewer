26473d75964f8fda859eddc28093e46533b41bb1
Jay Lee <j.lee2@adheretech.com>
2021-05-14 08:51:12 -0700

    icons for freezes and jumps off mods

--- src/components/chart/ModDisplay.js
+++ src/components/chart/ModDisplay.js
@@ -18,6 +18,12 @@ options.mods.turn.forEach((turnMod) => {
   icons[turnMod] = new Image()
   icons[turnMod].src = getAssetPath(`mods/${imageName}.png`)
 })
+icons['cut1'] = new Image()
+icons['cut1'].src = getAssetPath(`mods/cut_on.png`)
+icons['freezesOff'] = new Image()
+icons['freezesOff'].src = getAssetPath(`mods/freeze_arrow_off.png`)
+icons['jumpsOff'] = new Image()
+icons['jumpsOff'].src = getAssetPath(`mods/jump_off.png`)
 
 const ModDisplay = (props) => {
   const { mods } = props
@@ -47,11 +53,30 @@ const ModDisplay = (props) => {
     return <img src={icons[mods.turn].src} />
   }
 
+  const renderCutMod = () => {
+    return null
+  }
+  const renderFreezesMod = () => {
+    if (mods.freezes === 'off') {
+      return <img src={icons['freezesOff'].src} />
+    }
+    return null
+  }
+  const renderJumpsMod = () => {
+    if (mods.jumps === 'off') {
+      return <img src={icons['jumpsOff'].src} />
+    }
+    return null
+  }
+
   return (
     <div className="mod-display-container">
       <div className="mods-list">
         <div className="mod-wrapper">{renderSpeedMod()}</div>
         <div className="mod-wrapper">{renderTurnMod()}</div>
+        <div className="mod-wrapper">{renderCutMod()}</div>
+        <div className="mod-wrapper">{renderFreezesMod()}</div>
+        <div className="mod-wrapper">{renderJumpsMod()}</div>
       </div>
     </div>
   )
