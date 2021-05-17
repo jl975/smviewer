f39b5c422fb5e80f98f6d64b41f3f10caa6acd6e
Jay Lee <j.lee2@adheretech.com>
2021-05-17 16:49:34 -0700

    add border option for guidelines mod

--- src/components/form/ModsForm.js
+++ src/components/form/ModsForm.js
@@ -341,8 +341,24 @@ const ModsForm = (props) => {
             })}
           </div>
 
-          <h4>Miscellaneous</h4>
           <div className="form-field">
+            <h4 className="form-label">Guidelines</h4>
+            {options.mods.guidelines.map((guidelines) => {
+              return (
+                <Radio
+                  key={`guidelines_${guidelines}`}
+                  label={capitalize(guidelines)}
+                  name="guidelines"
+                  value={guidelines}
+                  checked={mods.guidelines === guidelines}
+                  onChange={() => updateMods({ guidelines })}
+                />
+              )
+            })}
+          </div>
+
+          <h4>Miscellaneous</h4>
+          {/* <div className="form-field">
             <Checkbox
               toggle
               label="Guidelines"
@@ -350,7 +366,7 @@ const ModsForm = (props) => {
               checked={mods.guidelines}
               onChange={() => updateMods({ guidelines: !mods.guidelines })}
             />
-          </div>
+          </div> */}
           <div className="form-field">
             <Checkbox
               toggle
